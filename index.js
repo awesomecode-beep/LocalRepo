0dc4c2c9c73d640d4b340a4e638a8bb088faa12a
awesomecode <ankithh354@gmail.com>
2024-01-18 21:25:17 +0530

    Add a file

---
+++ index.js
@@ -0,0 +1,104 @@
+/*let firstname = " Bro";
+let student = true;
+let age = 21
+
+age = age + 1;
+ 
+console.log('Hello',firstname);
+console.log('You are',age,'years old');
+console.log('Enrolled',student);
+
+document.getElementById('p1').innerHTML = "Hello" + firstname;
+document.getElementById('p2').innerHTML = "Hello" + firstname;
+document.getElementById('p3').innerHTML = "Hello" + firstname;
+
+let students = 20;
+students += 1; 
+let extras = students %2;
+console.log(extras)
+let username;
+
+document.getElementById("mybutton").onclick = function(){
+    username = document.getElementById("mytext").value;
+    console.log(username);
+    document.getElementById('myLabel').innerHTML = "Hello" + username;
+}
+
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy Birth Day! u are",age);
+
+let x = 3.187654;
+
+x = Math.round(x);
+console.log(x);
+
+let a;
+let b;
+let c;
+
+a = window.prompt("Enter side A");
+a = Number(a);
+
+b = window.prompt("Enter side B");
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+console.log("Side C",c);  
+
+let temp =-15; 
+
+if(temp<=0 && temp>=30)
+{
+    console.log("The weather is bad");
+}
+else{
+    console.log("The weather is good");
+}
+
+document.getElementById('mybutton').onclick = function(){
+
+    //const mycheck = document.getElementById('mycheckbox');
+
+    const visbtn = document.getElementById("visabtn");
+    const master = document.getElementById("mastercardbtn");
+    const paypal = document.getElementById("paypalbtn");
+
+
+    if(visbtn.checked)
+    {
+        console.log("VISA");
+    }
+    else if(master.checked)
+    {
+        console.log("MasterCard")
+    }
+
+}
+
+let username;
+
+while(username == "")
+{
+    username = window.prompt("Enter your name");
+}
+console.log("Hello",username);
+
+for(let i = Math.random(); i<=50; i+=1){
+    console.log(i);
+}
+console.log("happy new Year");
+
+for (let i=1;i<=20;i+=1){
+    if(i==13)
+    {
+        break;
+    }
+    console.log(i);
+}*/
+
